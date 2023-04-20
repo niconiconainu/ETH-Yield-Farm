e24e8f0fa474cc1492d189726adf73b1e336fe0f
SHØ <neila@users.noreply.github.com>
2023-04-20 14:09:49 +0900

    update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,13 +9,15 @@ module.exports = {
   extends: ['standard', 'prettier', 'plugin:node/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {
     'node/no-unsupported-features/es-syntax': [
       'error',
       { ignores: ['modules'] },
     ],
+    'node/no-unsupported-features/node-builtins': ['warn'],
     'node/no-unpublished-require': [
       'warn',
       { allowModules: ['tailwind-scrollbar'] },
@@ -24,6 +26,9 @@ module.exports = {
     'node/no-missing-import': ['warn'],
     'no-lone-blocks': ['off'],
     'no-unused-vars': ['warn'],
+    'react/prop-types': ['off'],
+    'react/jsx-uses-react': ['off'],
+    'react/react-in-jsx-scope': ['off'],
   },
   settings: {
     node: { tryExtensions: ['.js', '.json', '.node', '.ts', '.d.ts'] },
