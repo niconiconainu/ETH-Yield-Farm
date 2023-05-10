1d21dd2e3be65a53fb799a75c5b3cebf05a1d5a7
github honganji <yujicavs@icloud.com>
2023-05-11 08:39:42 +0900

    complete contract test

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -1,6 +1,19 @@
 require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { PRIVATE_KEY, STAGING_ALCHEMY_KEY } = process.env;
+
 module.exports = {
   solidity: '0.8.18',
+  networks: {
+    sepolia: {
+      url: STAGING_ALCHEMY_KEY || '',
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : ['0'.repeat(64)],
+    },
+    // mainnet: {
+    //   chainId: 1,
+    //   url: process.env.PROD_ALCHEMY_KEY,
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+  },
 };
